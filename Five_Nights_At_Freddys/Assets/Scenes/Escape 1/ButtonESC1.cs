690f9eead9edb3a439b30ca2b90bd5ce503b0bfe
Ahmed <ahmedmahmoudkzm@gmail.com>
2024-11-13 01:54:02 +0200

    Added jojo reference

--- Five_Nights_At_Freddys/Assets/Scenes/Escape 1/ButtonESC1.cs
+++ Five_Nights_At_Freddys/Assets/Scenes/Escape 1/ButtonESC1.cs
@@ -4,8 +4,18 @@ using UnityEngine.UI;
 public class ButtonESC1 : MonoBehaviour
 {
     public Button mainMenuButton;
+    public GameObject imageObject;
+    public void ShowImage()
+    {
+        imageObject.SetActive(true);
+    }
+    public void HideImage()
+    {
+        imageObject.SetActive(false);
+    }
     void Start()
     {
+        HideImage();
         mainMenuButton.onClick.AddListener(() =>
         {
             Time.timeScale = 1;
