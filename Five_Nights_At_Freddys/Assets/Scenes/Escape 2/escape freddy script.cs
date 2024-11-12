9a8251c37718da6609dc58fc9272994b604cc0e6
YoussefTamerr <yousseftamer1990@gmail.com>
2024-11-12 11:34:04 +0200

    added buttons to escape freddy scene

---
+++ Five_Nights_At_Freddys/Assets/Scenes/Escape 2/escape freddy script.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class escapefreddyscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Button mainMenuButton;
+    public GameObject choicePanel;
+
+    float timer = 0f;
+    bool timeStarted = false;
+    void Start()
+    {
+        timeStarted = true;
+        choicePanel.SetActive(false);
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1;
+            // UnityEngine.SceneManagement.SceneManager.LoadScene("main menu");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*if (timeStarted) WIN SCREEN
+        {
+            timer += Time.deltaTime;
+            if (timer >= 25)
+            {
+                choicePanel.SetActive(true);
+                Time.timeScale = 0;
+                timeStarted = false;
+            }
+        }*/
+
+    }
+}
