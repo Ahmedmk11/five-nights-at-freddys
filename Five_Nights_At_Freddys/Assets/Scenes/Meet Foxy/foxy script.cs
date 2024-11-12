90ff5d1dc3a9be49e934fcd1df3c100930f91a70
YoussefTamerr <yousseftamer1990@gmail.com>
2024-11-12 11:28:39 +0200

    added buttons to start and foxy scenes

---
+++ Five_Nights_At_Freddys/Assets/Scenes/Meet Foxy/foxy script.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class foxyscript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject choicePanel;
+    public Button freddyButton;
+    public Button escapeButton;
+    public Button mainMenuButton;
+
+    float timer = 0f;
+    bool timeStarted = false;
+    void Start()
+    {
+        timeStarted = true;
+        choicePanel.SetActive(false);
+        freddyButton.onClick.AddListener(() =>
+        {
+            choicePanel.SetActive(false);
+            Time.timeScale = 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MeetFreddyScene");
+        });
+        escapeButton.onClick.AddListener(() =>
+        {
+            choicePanel.SetActive(false);
+            Time.timeScale = 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("EscapeFreddyScene");
+        });
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            choicePanel.SetActive(false);
+            Time.timeScale = 1;
+            // UnityEngine.SceneManagement.SceneManager.LoadScene("main menu");
+        });
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timeStarted)
+        {
+            timer += Time.deltaTime;
+            if (timer >= 34.5)
+            {
+                choicePanel.SetActive(true);
+                Time.timeScale = 0;
+                timeStarted = false;
+            }
+        }
+
+    }
+}
