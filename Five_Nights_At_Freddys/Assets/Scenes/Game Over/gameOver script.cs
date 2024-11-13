7f57c67a47ee1ce66264985be2fc2633346e60b3
YoussefTamerr <yousseftamer1990@gmail.com>
2024-11-13 23:08:26 +0200

    minor fixes

--- Five_Nights_At_Freddys/Assets/Scenes/Game Over/gameOver script.cs
+++ Five_Nights_At_Freddys/Assets/Scenes/Game Over/gameOver script.cs
@@ -2,14 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class gameOverscript : MonoBehaviour
 {
     public TMP_Text gameOver;
+    public Button mainMenuButton;
     // Start is called before the first frame update
     void Start()
     {
         gameOver.SetText(gameOverText.text);
+        mainMenuButton.onClick.AddListener(() =>
+        {
+            Time.timeScale = 1;
+            UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenuScene");
+        });
     }
 
     // Update is called once per frame
